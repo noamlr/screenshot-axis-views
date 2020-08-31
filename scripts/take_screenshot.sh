e8211bc71dd438576fe8629ecb57163900c98948
noamlr <nlapar@uni.pe>
2020-08-30 22:37:52 -0300

    Add script for read image automatically

--- scripts/take_screenshot.sh
+++ scripts/take_screenshot.sh
@@ -1,6 +1,8 @@
 SCRIPT_EXEC=/home/guilherme/Documents/noa/cidia19/screenshot-axis-views/build/screenshot-axis-views
-INPUT_3D_IMAGE_PATH=/home/guilherme/Documents/noa/cidia19/data/output/exame-pulmao/
-OUTPUT_2D_SLICES_PATH=/home/guilherme/Documents/noa/cidia19/data/output-2d/exame-pulmao-2/
+#INPUT_3D_IMAGE_PATH=/home/guilherme/Documents/noa/cidia19/data/NII-HMV-OUT/exame-pulmao/
+INPUT_3D_IMAGE_PATH=/home/guilherme/Documents/noa/cidia19/data/NII-HCPA-OUT/exame-pulmao/
+#OUTPUT_2D_SLICES_PATH=/home/guilherme/Documents/noa/cidia19/data/output-2d/HMV/exame-pulmao/
+OUTPUT_2D_SLICES_PATH=/home/guilherme/Documents/noa/cidia19/data/output-2d/HCPA/exame-pulmao/
 TRANSFER_FUNCTION_PATH=/home/guilherme/Documents/noa/cidia19/data/TF/tf12_2.xml
 
 array_axis=( "axis1" "axis2" "axis3" "axis4" )
@@ -32,7 +34,7 @@ then
 	 do
 	 		# echo $
 			array_split=(${i//\// })
-			# echo ${array_split[8]}
+			# echo ${array_split[8]} 
 			OUTPUT_PATH="$OUTPUT_2D_SLICES_PATH${array_split[8]}/"
 			echo $OUTPUT_PATH
 			$SCRIPT_EXEC -tf $TRANSFER_FUNCTION_PATH -i $i -o $OUTPUT_PATH
